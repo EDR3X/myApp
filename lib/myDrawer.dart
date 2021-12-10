d8f13a1dbebb267f933903c6ca97f0a9d5bbc830
EDR3X <dhunganaannanta@gmail.com>
2021-12-10 19:02:28 +0545

    made changes to drawer i.e. made it better

--- lib/myDrawer.dart
+++ lib/myDrawer.dart
@@ -15,78 +15,81 @@ class myDrawer extends StatefulWidget {
 class _myDrawerState extends State<myDrawer> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Colors.teal,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            // UserAccountsDrawerHeader(
-            //     accountName: Text("Obi Wan"),
-            //     accountEmail: Text("obiwan@kinobi.com")),
-            const SizedBox(
-              height: 50,
-            ),
-            ListTile(
-              title: const Text("Home"),
-              leading: const Icon(Icons.home),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const homepage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text("First Page"),
-              leading: const Icon(Icons.live_help_outlined),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const firstPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text("Second Page"),
-              leading: const Icon(Icons.live_help_outlined),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const secondPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: const Text("third page"),
-              leading: const Icon(Icons.live_help_outlined),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const thirdPage()),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Divider(
-              color: Colors.white60,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ListTile(
-              title: const Text("Tic Tac Toe"),
-              leading: const Icon(Icons.games),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-              },
-            ),
-          ],
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.40,
+      child: Drawer(
+        backgroundColor: Colors.teal,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: ListView(
+            children: [
+              // UserAccountsDrawerHeader(
+              //     accountName: Text("Obi Wan"),
+              //     accountEmail: Text("obiwan@kinobi.com")),
+              const SizedBox(
+                height: 50,
+              ),
+              ListTile(
+                title: const Text("Home"),
+                leading: const Icon(Icons.home),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const homepage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text("First Page"),
+                leading: const Icon(Icons.live_help_outlined),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const firstPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text("Second Page"),
+                leading: const Icon(Icons.live_help_outlined),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const secondPage()),
+                  );
+                },
+              ),
+              ListTile(
+                title: const Text("third page"),
+                leading: const Icon(Icons.live_help_outlined),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const thirdPage()),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(
+                color: Colors.white60,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ListTile(
+                title: const Text("Tic Tac Toe"),
+                leading: const Icon(Icons.games),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
